b2938ddb8b796136f4a703387646f50d85669265
Marlon Santos <marlonergondossantos@gmail.com>
2020-04-08 23:15:13 -0300

    create pages module

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,9 @@
+import { ControlePerdasModule } from "./controle-perdas/controle-perdas.module";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ControlePerdasModule],
+})
+export class PagesModule {}
