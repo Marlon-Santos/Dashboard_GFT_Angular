15c9e6c902f5dc568652bf02d96ae2c9ac6e19c1
Marlon Santos <marlonergondossantos@gmail.com>
2020-04-08 23:14:03 -0300

    add header in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { ControlePerdasModule } from "./controlePerdas/controle-perdas.module";
+import { PagesModule } from "./pages/pages.module";
+import { CoreModule } from "./core/core.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ControlePerdasModule],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, CoreModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
